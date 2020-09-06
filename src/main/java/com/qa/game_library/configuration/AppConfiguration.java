be63f9f24fbdc64dac50ff4c7bd464d43588de5d
Paulo Silva <Psilva@qa.com>
2020-09-06 18:17:38 +0100

    changed port to 9000

--- src/main/java/com/qa/game_library/configuration/AppConfiguration.java
+++ src/main/java/com/qa/game_library/configuration/AppConfiguration.java
@@ -3,13 +3,22 @@ package com.qa.game_library.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
+@EnableWebMvc
 public class AppConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler( "/js/**")
+                .addResourceLocations("classpath:/static/js/");
     }
 
 }
